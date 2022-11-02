831490ae76df2e746372320b8e2e0038ab7ea831
Yoochanhong <chanhongy6@naver.com>
2022-11-02 22:04:34 +0900

    ⚜️ :: 상단 소개 글 추가

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -8,8 +8,60 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool isSignupScreen = true;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            child: Container(
+              padding: EdgeInsets.only(
+                top: 90,
+                left: 42,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      text: 'Welcome',
+                      style: TextStyle(
+                        letterSpacing: 1.0,
+                        fontSize: 20,
+                        color: Colors.black,
+                      ),
+                      children: [
+                        TextSpan(
+                          text: ' to Chanting app!',
+                          style: TextStyle(
+                            letterSpacing: 1.0,
+                            fontSize: 20,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    'Sign up continue',
+                    style: TextStyle(
+                      letterSpacing: 1.0,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
